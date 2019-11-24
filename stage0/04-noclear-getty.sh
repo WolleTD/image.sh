2e7a8c162fe10a4b503119baf0fcaa5d2ad98a1f
Eicke Herbertz <wolletd@posteo.de>
2019-11-24 21:50:44 +0100

    Refactor stage1 into stage0 and stage2

--- stage0/04-noclear-getty.sh
+++ stage0/04-noclear-getty.sh
@@ -3,13 +3,3 @@ FILES=${BASH_SOURCE##*/}.d
 install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 ${FILES}/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 ${FILES}/fstab "${ROOTFS_DIR}/etc/fstab"
-
-on_chroot << EOF
-if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
-	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
-fi
-echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
-echo "root:root" | chpasswd
-EOF
-
-
---
+++ stage2/16-adduser.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+on_chroot << EOF
+if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
+	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
+fi
+echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
+echo "root:root" | chpasswd
+EOF
+
+
