eea69125d5fd646ae288b8bba3bbb865b06e4c71
Eicke Herbertz <wolletd@posteo.de>
2019-12-09 19:29:20 +0100

    Configure apt proxy

--- stage0/01-configure-apt.sh
+++ stage0/01-configure-apt.sh
@@ -5,8 +5,8 @@ install -m 644 ${FILES}/sources.list "${ROOTFS_DIR}/etc/apt/"
 install -m 644 ${FILES}/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
 if [ -n "$APT_PROXY" ]; then
-	install -m 644 ${FILES}/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
-	sed "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache" -i -e "s|APT_PROXY|${APT_PROXY}|"
+    sed -e "s|@APT_PROXY@|${APT_PROXY}|" ${FILES}/51cache.in > \
+        "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache" 
 else
 	rm -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 fi
