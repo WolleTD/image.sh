27fd9ea9a752d91db630287c683f462be9cafff8
Eicke Herbertz <wolletd@posteo.de>
2019-12-15 23:25:05 +0100

    Move files into rootfs layout

--- stage0/02-boot-files.sh
+++ stage0/02-boot-files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
-export FILES=${BASH_SOURCE##*/}.d
-install -m 644 ${FILES}/cmdline.txt "${ROOTFS_DIR}/boot/"
-install -m 644 ${FILES}/config.txt "${ROOTFS_DIR}/boot/"
+FILENAME="boot/cmdline.txt"
+install -m 644 files/${FILENAME} "${ROOTFS_DIR}/${FILENAME}"
+FILENAME="boot/config.txt"
+install -m 644 files/${FILENAME} "${ROOTFS_DIR}/${FILENAME}"
