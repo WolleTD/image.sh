917ccfffd505cdf5bcde6c129b525e857591027b
Eicke Herbertz <wolletd@posteo.de>
2019-12-17 01:08:54 +0100

    Imagefile for export-image

--- export-image/02-set-partuuid.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-${BASE_DIR}/scripts/genfstab -t PARTUUID "${ROOTFS_DIR}" | tee "${ROOTFS_DIR}/etc/fstab"
-PARTUUID=$(lsblk -rno PARTUUID $(findmnt -no SOURCE "${ROOTFS_DIR}"))
-
-sed -i "s/ROOTDEV/PARTUUID=${PARTUUID}/" "${ROOTFS_DIR}/boot/cmdline.txt"
